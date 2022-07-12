402b01fd4afda3b87e229b6c1c64799b5d5acb7e
gorimatyan <gorima102131@gmail.com>
2022-07-13 05:34:09 +0900

    mapでの画像の出力ができた

--- components/Main.tsx
+++ components/Main.tsx
@@ -4,20 +4,42 @@ import { useRef, useEffect, ChangeEvent, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Main: NextPage = () => {
+    // 出力する画像のURLを入れる変数
+    const [imageURLs, setImageURLs] = useState<string[]>([])
+    // canvasの要素を所得
     const canvasRef = useRef(null);
+    // canvasRefからgetContextを返す関数
     const getContext = (): CanvasRenderingContext2D => {
-       const canvas: any = canvasRef.current;
-
-       return canvas.getContext('2d');
+        const canvas: any = canvasRef.current;
+        const toData = canvas.toDataURL();
+        return canvas.getContext('2d');
     }
+    // canvasRefの要素をそのまま返す関数
+    const getCanvas = ()=> {
+        const canvas: any = canvasRef.current;
+        return canvas;
+    }
+    // ↓画像ファイルのonChangeで使用
+    // 選択された画像ファイルのObjectURLを突っ込んだImageオブジェクトを返す関数
+    const setNewImage = (e: ChangeEvent<HTMLInputElement>): HTMLImageElement => {
+        const newImage = new Image;
+        const ctx = getCanvas();
+        if(e.target.files){
+            const imageURL: string = window.URL.createObjectURL(e.target.files[0])
+            setImageURLs((prev) => {
+                return [...prev, imageURL]
+            });
+            newImage.src = imageURL;
+        }
+        return newImage;
+    }
+    // onChange毎にsetImageで取得した画像を描写する関数
     const handleFiles = (e: ChangeEvent<HTMLInputElement>) => {
         const ctx: CanvasRenderingContext2D = getContext();
-        // ctx.drawImage();
-    }
-    const getNewImage = () => {
-        const newImage = new Image;
-
-        return newImage;
+        const img = setNewImage(e);
+        img.onload = () => {
+            ctx.drawImage(img,0,0);
+        }
     }
 
     return (
@@ -39,10 +61,10 @@ const Main: NextPage = () => {
             </div>
 
             <div className="outputArea">
-                {imagesAllURLs.map((URL: string): any => 
+                {imageURLs.map((URL: string) =>
                     <img key={URL} src={URL} alt="" />
                 )}
-                
+
             </div>
         </main>
     );
